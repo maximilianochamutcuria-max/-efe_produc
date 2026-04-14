e953290324912322e7243d35c475cd4d3469b913
Maximliano Chamut <maxi@MacBook-Pro-de-Maximliano.local>
2026-04-14 10:33:31 -0300

    automatizacion funcionando

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
+const multer = require("multer");
 const { MercadoPagoConfig, Preference } = require("mercadopago");
 const { createClient } = require("@supabase/supabase-js");
 
