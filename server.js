a780d4bc75b4e1ddaa30372a63bb9a3a1fc7e8ec
Maximliano Chamut <maxi@MacBook-Pro-de-Maximliano.local>
2026-04-23 18:48:56 -0300

    fix puerto render

--- server.js
+++ server.js
@@ -756,6 +756,8 @@ app.post("/webhook", async (req, res) => {
   }
 });
 // 🔥 ARRANCAR SERVIDOR
-app.listen(3000, () => {
-  console.log("🔥 http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("🔥 Servidor corriendo en puerto " + PORT);
 });
